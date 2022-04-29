aaa2613910a02763b84edd5e744bd1b7f8f7cc1c
TerM1d <tda1110@mail.ru>
2022-04-29 17:22:49 +0700

    del

--- module5/f2.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include "mylib.h"
-int f2(int a, int b) {
-        return a - b;
-}
--- module5/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "mylib.h"
-
-int main() {
-	printf("%d\n", f1(1, 3));
-	printf("%d\n", f2(1, 3));
-	return 0;
-}
