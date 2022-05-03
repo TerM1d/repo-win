01b616b63e2d1c7b0e523151c65f479b340f98ff
TerM1d <tda1110@mail.ru>
2022-05-03 14:34:52 +0700

    change main

--- module5/lab1/main.c
+++ module5/lab1/main.c
@@ -5,7 +5,6 @@
 int main(int argc, char* argv[]) {
 	void *ext_library = dlopen("./libfsdyn.so",RTLD_LAZY);
 	if (!ext_library){
-		//если ошибка, то вывести ее на экран
 		fprintf(stderr, "dlopen() error: %s\n", dlerror());
 		return 1;
 	}
@@ -15,13 +14,11 @@ int main(int argc, char* argv[]) {
 	int (*powerfunc2)(int x, int y);
 	powerfunc2 = dlsym(ext_library, "f2");
 
-	//выводим результат работы процедуры
+	
 	printf("%d\n", (*powerfunc1)(5, 8));
 	printf("%d\n", (*powerfunc2)(5, 8));
 
-	//закрываем библиотеку
 	dlclose(ext_library);
-	//printf("%d\n", f1(1, 3));
-	//printf("%d\n", f2(1, 3));
+
 	return 0;
 }
